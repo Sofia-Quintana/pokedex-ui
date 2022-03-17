20f5fddfed703f5584fea3901bcc0836d33c1486
Sofia-Quintana <sofia.quintana@telus.com>
2022-03-16 22:03:04 -0600

    feat: added home body structure

---
+++ src/pages/home/components/Body.js
@@ -0,0 +1,7 @@
+import { BodyContainer } from "./styles/Body.styles";
+ 
+const Body = ({ children }) => {
+    return <BodyContainer> { children } </BodyContainer>
+};
+
+export default Body;
---
+++ src/pages/home/containers/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Body from "../components/Body";
+import { Container } from "../components/styles/Home.styles";
+
+export const Home = () => {
+    return(
+        <Container>
+            <Body>
+
+            </Body>
+        </Container>
+    ); 
+}
+
+export default Home;
